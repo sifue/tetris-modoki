a6763c470d219ef66160cc9eb0d5081e048ad62f
Soichiro Yoshimura <yoshimura@soichiro.org>
2017-05-04 03:06:05 +0900

    add key event loop

--- tetrisModoki.js
+++ tetrisModoki.js
@@ -1,22 +1,24 @@
-(function(){
+(function () {
     'use strict';
 
     const container = document.querySelector('#container');
     const table = document.createElement('table');
     const rowCount = 22;
     const columnCount = 10;
-    const startPosition = {x:4, y:0};
-    const roopInterval = 1000;
+    const startPosition = { x: 4, y: 0 };
+    const roopInterval = 30;
+    const moveInterval = 1000;
     const state = {
         fallingBlock: null,
-        groundedCells: []
+        groundedCells: [],
+        lastMovedTime: null
     };
     const blocks = [
-        [{x:0, y:0}, {x:1, y:0}, {x:0, y:1}, {x:1, y:1}]
+        [{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 0, y: 1 }, { x: 1, y: 1 }]
     ];
-    let cells = []; // y, x の順のインデックス指定
+    let cells = []; // y, x order index ex. cells[y][x]
 
-    function createTable(){
+    function createTable() {
         for (let i = 0; i < rowCount; i++) {
             const tr = document.createElement('tr');
             for (let j = 0; j < columnCount; j++) {
@@ -39,7 +41,7 @@
             cells.push(row);
         }
     }
-    
+
     function mainLoop() {
         render();
         setTimeout(mainLoop, roopInterval);
@@ -49,40 +51,49 @@
         clearCells();
 
         let block = null;
-        if(!state.fallingBlock) {
+        if (!state.fallingBlock) {
             block = [];
             const originBlock = blocks[0]; // TODO get random
             // set starting position and copy
             for (let i = 0; i < originBlock.length; i++) {
                 block.push({
-                    x : (originBlock[i].x + startPosition.x),
-                    y : (originBlock[i].y + startPosition.y)
-                  })
+                    x: (originBlock[i].x + startPosition.x),
+                    y: (originBlock[i].y + startPosition.y)
+                })
             }
-          } else {
-            const moveBlock = state.fallingBlock;
-            // check grounding
-            if (isGrounding(moveBlock)) {
-                // remove
-                state.groundedCells = state.groundedCells.concat(moveBlock);
-                block = null;
-                state.fallingBlock = null;
-            } else {
-                // move
-                for (let i = 0; i < moveBlock.length; i++) {
-                    moveBlock[i].y = moveBlock[i].y + 1;
+            state.lastMovedTime = new Date().getTime();
+        } else {
+
+            const now = new Date();
+            if (canMove(now)) {
+                const moveBlock = state.fallingBlock;
+                // check grounding
+                if (isGrounding(moveBlock)) {
+                    // remove
+                    state.groundedCells = state.groundedCells.concat(moveBlock);
+                    block = null;
+                    state.fallingBlock = null;
+                } else {
+                    // move
+                    for (let i = 0; i < moveBlock.length; i++) {
+                        moveBlock[i].y = moveBlock[i].y + 1;
+                    }
+                    block = moveBlock;
                 }
-                block = moveBlock;
+                state.lastMovedTime = now.getTime();
+            } else {
+                // not move
+                block = state.fallingBlock;
             }
         }
 
         // set falling block to cells
-        if(block) {
+        if (block) {
             for (let i = 0; i < block.length; i++) {
-                cells[block[i].y][block[i].x] = true; 
+                cells[block[i].y][block[i].x] = true;
             }
             state.fallingBlock = block;
-        } 
+        }
 
         // set grounded cells to cells 
         for (let i = 0; i < state.groundedCells.length; i++) {
@@ -95,8 +106,8 @@
     function renderTable() {
         for (let i = 0; i < rowCount; i++) {
             for (let j = 0; j < columnCount; j++) {
-                const td = document.querySelector('#cell-' + j + '-' + i );
-                if(cells[i][j]) {
+                const td = document.querySelector('#cell-' + j + '-' + i);
+                if (cells[i][j]) {
                     td.classList.add('exists');
                 } else {
                     td.classList.remove('exists');
@@ -114,13 +125,13 @@
                 y: moveBlock[i].y + 1
             };
 
-            if(fallenCell.y >= rowCount) {
+            if (fallenCell.y >= rowCount) {
                 isGrounded = true;
                 break;
             }
 
             for (let j = 0; j < state.groundedCells.length; j++) {
-                if(state.groundedCells[j].x === fallenCell.x
+                if (state.groundedCells[j].x === fallenCell.x
                     && state.groundedCells[j].y === fallenCell.y
                 ) {
                     isGrounded = true;
@@ -129,6 +140,10 @@
             }
         }
         return isGrounded;
+    }
+
+    function canMove(now) {
+        return (now.getTime() - state.lastMovedTime) >= moveInterval;
     }
 
     createTable();
